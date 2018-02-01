8fbbc387e47f03da1f311c9a70f3b3b8240bbde1
David Dinh <ddavid@biospace.local>
2018-02-01 18:13:39 +0000

    added input validation

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, Response, json, jsonify
+from validate_input import is_valid
+
  
 app = Flask(__name__)
  
 @app.route('/')
 def index():
-	return 'Hello World'
+	return 'Hello Lyft!'
 
 @app.route("/test", methods = ["POST"])
 def api_message():
@@ -12,19 +14,21 @@ def api_message():
         if request.is_json:
             # set data var to json request object
             data = request.json
-            # build a new object
-            total_dict = {}
-            #set value to "sum" key
-            total_dict["sum"] = sum(data.values())
-            #encode to json
-            json_data = json.dumps(total_dict)
-            # return json object
-            return json_data
+            #validate input values
+            if is_valid(data):
+                # build a new object
+                total_dict = {}
+                #set value to "sum" key
+                total_dict["sum"] = sum(data.values())
+                #encode to json
+                json_data = json.dumps(total_dict)
+                # return json object
+                return json_data
         else:
             return "Invalid JSON data"
     except (TypeError, NameError):
         print("There was an error with your JSON input, please try again.")
-        
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
---
+++ validate_input.py
@@ -0,0 +1,23 @@
+import numbers
+
+def is_valid(my_dict):
+    """
+    Check to see if the input dictionary is a correct input
+    :param my_dict: input dictionary
+    :return: boolean, True if valid, False if invalid
+    """
+
+    input_fields = ["x", "y"]
+
+    # Iterate through fields e.g. 'x', and 'y'
+    for field in input_fields:
+
+        # Make sure we have the correct input fields by iterating though all fields we want to check
+        if field not in my_dict.keys():
+            return False
+
+        # Make sure the values in the field are numbers
+        if not isinstance(my_dict[field], numbers.Number):
+            return False
+
+    return True
